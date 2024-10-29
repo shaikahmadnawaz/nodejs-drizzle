b41d9a1e4234749f66733a05ef630a2e091c68e6
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-10-29 07:30:08 +0530

    feat(orders): written placeOrder, getOrderDetailsById APIs

--- src/controllers/order.controllers.ts
+++ src/controllers/order.controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 import { db } from "../db";
-import { orderTable } from "../db/schema";
+import { goldStoreTable, orderTable } from "../db/schema";
 import { eq } from "drizzle-orm";
 
 export const addOrder = async (req: Request, res: Response) => {
@@ -84,5 +84,63 @@ export const deleteOrder = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Error deleting order:", error);
     res.status(500).json({ error: "Failed to delete order" });
+  }
+};
+
+export const placeOrder = async (req: Request, res: Response) => {
+  try {
+    const { goldStoreId, quantity } = req.body;
+
+    const jewelryItem = await db
+      .select()
+      .from(goldStoreTable)
+      .where(eq(goldStoreTable.id, goldStoreId));
+    if (jewelryItem.length === 0) {
+      res.status(404).json({ error: "Jewelry item not found" });
+    }
+
+    const totalPrice = (Number(jewelryItem[0].price) * quantity).toString();
+
+    const newOrder = {
+      goldStoreId,
+      quantity,
+      totalPrice,
+    };
+
+    await db.insert(orderTable).values(newOrder);
+    res.status(201).json({ message: "Order placed successfully!" });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    res.status(500).json({ error: "Failed to place order" });
+  }
+};
+
+export const getOrderDetailsById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const orderDetails = await db
+      .select({
+        orderId: orderTable.id,
+        orderDate: orderTable.orderDate,
+        quantity: orderTable.quantity,
+        totalPrice: orderTable.totalPrice,
+        jewelryName: goldStoreTable.jewelryName,
+        jewelryType: goldStoreTable.jewelryType,
+        weight: goldStoreTable.weight,
+        price: goldStoreTable.price,
+      })
+      .from(orderTable)
+      .innerJoin(goldStoreTable, eq(orderTable.goldStoreId, goldStoreTable.id))
+      .where(eq(orderTable.id, parseInt(id)));
+
+    if (orderDetails.length === 0) {
+      res.status(404).json({ error: "Order not found" });
+    }
+
+    res.status(200).json({ order: orderDetails[0] });
+  } catch (error) {
+    console.error("Error retrieving order details:", error);
+    res.status(500).json({ error: "Failed to retrieve order details" });
   }
 };
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from "express";
 import {
-  addOrder,
   deleteOrder,
   getOrderById,
+  getOrderDetailsById,
   getOrders,
+  placeOrder,
   updateOrder,
 } from "../controllers/order.controllers";
 
 const router = Router();
 
-router.route("/orders").get(getOrders).post(addOrder);
+router.route("/orders").get(getOrders).post(placeOrder);
 router
   .route("/orders/:id")
   .get(getOrderById)
   .put(updateOrder)
   .delete(deleteOrder);
+router.get("/orders/details/:id", getOrderDetailsById);
 
 export default router;
